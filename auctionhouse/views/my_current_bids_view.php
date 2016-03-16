279e02593315b165b3a0f8c3459f1c66a3bb7627
SickAustrian <SickAustrian@users.noreply.github.com>
2016-03-16 15:55:37 +0000

    open live auction bug fix

--- auctionhouse/views/my_current_bids_view.php
+++ auctionhouse/views/my_current_bids_view.php
@@ -83,7 +83,6 @@ $liveWithBidAuctions= QueryOperator::getLiveAuctionsWhereBuyerHasBid($user->getU
                         echo "<h4>There are no unfinished auctions you have bid on</h4>";
                     } else {
                         foreach ( $liveWithBidAuctions as $auction ) {
-                            echo $auction -> getSellerUsername();
                             $origin = "liveWithBid";
                             include "../includes/live_auction_to_buyer.php";
                         }
