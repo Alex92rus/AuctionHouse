1565505e9887c19df764b41f0e38a654db9e3dd3
SickAustrian <SickAustrian@users.noreply.github.com>
2016-03-17 00:29:09 +0000

    footer update

--- auctionhouse/includes/footer.php
+++ auctionhouse/includes/footer.php
@@ -1,7 +1,11 @@
+<?php
+$date = new DateTime( "now", new DateTimeZone( TIMEZONE ) );
+?>
+
 <div class="footer">
     <div class="container">
         <div class="navbar-text pull-left">
-            <p>Copyright &copy; <?php echo date( "Y", time() ); ?> AuctionHouse</p>
+            <p>Copyright &copy; <?= $date -> format( "Y" ) ?> AuctionHouse</p>
         </div>
         <div class="navbar-text pull-right">
             <p><a href="#">About</a> |  <a href="#">Contact</a> |  <a href="#">Privacy & Cookies</a> |  <a href="#">Developers</a></p>
